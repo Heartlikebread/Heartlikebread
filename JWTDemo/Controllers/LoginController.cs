1b9af88228ffdb61015c883feb9a3fbb29b7e655
彥廷 柯 <dku04wu6@gmail.com>
2020-07-29 10:56:02 +0800

    IRepository練習

--- JWTDemo/Controllers/LoginController.cs
+++ JWTDemo/Controllers/LoginController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using JWTDemo.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -31,13 +34,14 @@ namespace JWTDemo.Controllers
                 var tokenStr = GenerteJSONWebToken(user);
                 response = Ok(new { token = tokenStr });
             }
+            return response;
         }
         private UserModel AuthenticateUser(UserModel login)
         {
             UserModel user = null;
-            if (login.UserName == "test" && login.PassWord == "test"){ 
-            
-                user=new UserModel { UserName="test",PassWord="test",EmailAddress="test.email.com"}
+            if (login.UserName == "test" && login.PassWord == "test"){
+
+                user = new UserModel { UserName = "test", PassWord = "test", EmailAddress = "test.email.com" };
             }
             return user;
 
@@ -45,6 +49,37 @@ namespace JWTDemo.Controllers
 
         private string GenerteJSONWebToken(UserModel userinfo) {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+        new Claim(JwtRegisteredClaimNames.Sub,userinfo.UserName),
+        new Claim(JwtRegisteredClaimNames.Email,userinfo.EmailAddress),
+         new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: Configuration["Jwt:Issuer"],
+                audience: Configuration["Jwt:Issuer"],
+            claims,
+            expires: DateTime.Now.AddMinutes(120),
+            signingCredentials: credentials);
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post() {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcome To：" + userName;
+        }
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get() {
+            return new string[] { "Valuel", "Value2", "Value3" };
+        
         }
     }
 }
